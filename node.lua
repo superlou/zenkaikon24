c8710ef02024626ed5b73ae6ce60a554e6e33f95
superlou <lousimons@gmail.com>
2023-12-13 20:11:25 -0500

    WIP

--- node.lua
+++ node.lua
@@ -3,5 +3,5 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 local font = resoure.load_font "fonts/Lato-Regular.ttf"
 
 function node.render()
-    font:write(250, 300, "Hello, World", 64, 1, 1, 1, 1)
+    font:write(250, 300, "Hello, World", 64, 0, 1, 1, 1)
 end
