89f442bbb0c8653c1084c0c96518881a02fe0c59
superlou <lousimons@gmail.com>
2023-12-17 15:34:55 -0500

    Adding backgrounds

--- heading.lua
+++ heading.lua
@@ -1,7 +1,22 @@
--- local heading_bg = resource.create_colored_texture()
+require "color_util"
+require "text_util"
 
-function draw_heading(x, y, text, font, style)
-    font:write(x, y, text, 64, 1, 1, 1, 1)
+local heading_dark_bg = create_color_resource_hex("e91e63")
+local heading_dark_bg_shadow = create_color_resource_hex("bb114b")
+
+local heading_light_bg_shadow = create_color_resource_hex("333195")
+local light_r, light_g, light_b = hex2rgb("333195")
+local size = 64
+
+function draw_heading_dark(font, x, y, text)
+    local font_w = font:width(text, size)
+    heading_dark_bg:draw(x - font_w / 2 - 50, y - 24, x + font_w / 2 + 50, y + size + 16)
+    heading_dark_bg_shadow:draw(x - font_w / 2 - 50, y + size + 16, x + font_w / 2 + 50, y + size + 16 + 8)
+    write_centered(font, x, y, text, size, 1, 1, 1, 1)
 end
 
-return draw_heading
+function draw_heading_light(font, x, y, text)
+    local font_w = font:width(text, size)
+    heading_light_bg_shadow:draw(x - font_w / 2 - 50, y + size + 16, x + font_w / 2 + 50, y + size + 16 + 8)
+    write_centered(font, x, y, text, size, light_r, light_g, light_b, 1)
+end
--- node.lua
+++ node.lua
@@ -1,14 +1,22 @@
-gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+gl.setup(1920, 1080)
+require "heading"
 local Ticker = require "ticker"
-local draw_heading = require "heading"
+local offset = require "offset"
 
 local font = resource.load_font "font_Lato-Regular.ttf"
+local right_bg = resource.load_image "img_right_bg.png"
 
 local ticker = Ticker:new("data_ticker.json", 0, 800, WIDTH, 100)
 
 function node.render()
-    gl.clear(0, 0, 0, 1)
-    font:write(250, 300, "Zenkaikon 2024", 64, 1, 1, 1, 1)
-    draw_heading(1000, 300, "HAPPENING NOW", font, false)
+    gl.clear(1, 1, 1, 1)
+    right_bg:draw(903, 0, 903 + 1017, 1080)
+
+    draw_heading_light(font, 450, 100, "YOU ARE HERE")
+
+    offset(1030, 0, function ()
+        draw_heading_dark(font, 430, 100, "HAPPENING NOW")
+    end)
+
     ticker:draw()
 end
