c475195b182bcf8c741b7f2a987887ae237a3717
superlou <lousimons@gmail.com>
2024-03-04 20:10:16 -0500

    Added timezone form metadata

--- py_clock.py
+++ py_clock.py
@@ -1,12 +1,25 @@
 from datetime import datetime
+import time
+import pytz
 
 
-def determine_now(debug_datetime_text):
+def determine_now(debug_datetime_text, timezone):
     try:
+        # If we can parse the debug datetime, use that as local time.
         fmt = '%Y-%m-%dT%H:%M:%S'
         now = datetime.strptime(debug_datetime_text, fmt)
+        return now
     except Exception:
-        now = datetime.now()
+        pass
+    
+    is_dst = time.localtime().tm_isdst
+    tzname = time.tzname[is_dst]
+
+    now = datetime.now()
+
+    if tzname == "UTC":
+        # We are probably on a Raspberry Pi and need to convert to local time.
+        now = pytz.utc.localize(now).astimezone(timezone)
 
     return now
 
